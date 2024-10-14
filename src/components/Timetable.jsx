5509142c1c3fdf4458ef224b147dabfd3e62803e
dihbgs <dihborgesdr@gmail.com>
2024-10-14 12:19:30 -0300

    feature: add QoL stuff

--- src/components/Timetable.jsx
+++ src/components/Timetable.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function stringToTime(date) {
   return new Date(date).toLocaleTimeString("pt-BR", {
     hour: "2-digit",
@@ -39,15 +41,84 @@ const Card = ({ course }) => {
   );
 };
 
-const Timetable = ({ courses }) => {
-  const cards = courses.map((course, index) => {
-    return <Card key={index} course={course} />;
+const SearchBar = ({ setSearch }) => {
+  return (
+    <div className="flex flex-row justify-center w-full">
+      <input
+        className="border-2 border-emerald-300 dark:border-emerald-950 dark:text-emerald-50 dark:bg-emerald-950 outline-none bg-emerald-50 text-emerald-800 px-2"
+        type="text"
+        placeholder="Pesquise aqui"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+    </div>
+  );
+};
+
+const CoursesDropdown = ({ courses, setFilteredCourses }) => {
+  const types = courses.map((course) => course.type.toLowerCase());
+  const options = [...new Set(types)].map((type, index) => {
+    return (
+      <option key={index} value={type}>
+        {type}
+      </option>
+    );
   });
 
   return (
-    <div className="grid h-full grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-      {cards}
+    <div className="flex flex-row justify-center w-full">
+      <select
+        className="flex w-full border-2 border-emerald-300 dark:border-emerald-950 dark:text-emerald-50 dark:bg-emerald-950 outline-none p-2 text-emerald-800 bg-emerald-50"
+        onChange={(e) => {
+          const type = e.target.value;
+          const filtered = filterByType(courses, type);
+          setFilteredCourses(filtered);
+        }}
+      >
+        {options}
+      </select>
     </div>
+  );
+};
+
+function sortByDate(courses) {
+  return courses.sort((a, b) => {
+    return new Date(a.start) - new Date(b.start);
+  });
+}
+
+function filterByType(courses, type) {
+  return courses.filter((course) => course.type.toLowerCase() === type);
+}
+
+const Timetable = ({ courses }) => {
+  const [search, setSearch] = useState("");
+  const [filteredCourses, setFilteredCourses] = useState(sortByDate(courses));
+
+  const cards = filteredCourses.map((course, index) => {
+    const { title, speakers } = course;
+    const people = speakers.join(", ");
+    const searchValue = title + people + course.type + course.location;
+
+    if (searchValue.toLowerCase().includes(search.toLowerCase())) {
+      return <Card course={course} key={index} />;
+    }
+
+    return null;
+  });
+
+  return (
+    <>
+      <div className="flex flex-row justify-around p-4 gap-4">
+        <SearchBar setSearch={setSearch} />
+        <CoursesDropdown
+          setFilteredCourses={setFilteredCourses}
+          courses={courses}
+        />
+      </div>
+      <div className="grid h-full grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+        {cards}
+      </div>
+    </>
   );
 };
 
